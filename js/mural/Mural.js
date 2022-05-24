113562593b09ca0b70cbad1e3034f8ccd7ae7e92
Antony Maziero Warken <againplayi7@gmail.com>
2022-05-23 21:07:21 -0300

    push

---
+++ js/mural/Mural.js
@@ -0,0 +1,78 @@
+const Mural = (function(_render, Filtro){
+    "use strict"
+
+    let cartoes = pegaCartoesUsuario()
+    cartoes.forEach(cartao => {
+        preparaCartao(cartao)
+    })
+
+    const render = () => _render({cartoes: cartoes, filtro: Filtro.tagsETexto});
+    render()
+    Filtro.on("filtrado", render)
+
+    function pegaCartoesUsuario() {
+        let cartoesLocal = JSON.parse(localStorage.getItem(usuario))
+   
+        if(cartoesLocal) {
+            return cartoesLocal.map(cartaoLocal => new Cartao(cartaoLocal.conteudo, cartaoLocal.tipo))
+        } else {
+            return []
+        }
+    }
+
+
+    function preparaCartao(cartao){
+        const urlsImagens = Cartao.pegaImagens(cartao);
+        urlsImagens.forEach(url => {
+            fetch(url).then(resposta => {
+                caches.open('ceep-imagens').then(cache => {
+                    cache.put(url, resposta)
+                })
+            })
+        })
+        cartao.on("mudanca.**", salvaCartoes)
+        cartao.on("remocao", ()=>{
+            cartoes = cartoes.slice(0)
+            cartoes.splice(cartoes.indexOf(cartao),1)
+            salvaCartoes()
+            render()
+        })
+    }
+
+    function salvaCartoes(){
+        localStorage.setItem(usuario, JSON.stringify(
+            cartoes.map(cartao => ({conteudo: cartao.conteudo, tipo: cartao.tipo}))
+        ))
+    }
+
+    login.on('login', () => {
+        cartoes = pegaCartoesUsuario()
+        render()
+    })
+
+    login.on('logout', () => {
+        cartoes = []
+        render()
+    })
+
+    function adiciona(cartao){
+        if(logado){
+            cartoes.push(cartao)
+            salvaCartoes()
+            cartao.on("mudanca.**", render)
+            let listaImagens = Cartao.pegaImagens(cartao)
+            
+            preparaCartao(cartao)
+
+            render()
+            return true
+        } else {
+            alert("Você não está logado")
+        }
+    }
+
+    return Object.seal({
+        adiciona
+    })
+
+})(Mural_render, Filtro)
---
+++ service-worker.js
@@ -0,0 +1,60 @@
+let versao = 4
+/* versão 2 */
+
+let arquivos = ["/", 
+"css/estilos.css",
+"css/opcoesDaPagina.css",
+"css/opcoesDoCartao.css",
+"css/cabecalho.css",
+"css/login.css",
+"css/loginForm.css",
+"css/loginStatus.css",
+"css/cartao.css",
+"css/novoCartao.css",
+"css/mural.css",
+"js/lib/jquery.js",
+"js/lib/eventemitter2.js",
+"js/lib/KeyBoardNavigation.js",
+"js/tags/Tags.js",
+"js/cabecalho/mudaLayout.js",
+"js/cabecalho/busca.js",
+"js/filtro/Filtro.js",
+"js/tipos/TiposCartao.js",
+"js/cartao/render/Cartao_renderHelpers",
+"js/cartao/render/CartaoOpcoes_render",
+"js/cartao/render/CartaoConteudo_render",
+"js/cartao/render/Cartao_render",
+"js/cartao/Cartao.js",
+"js/login/LoginUsuario_render.js",
+"js/login/LoginUsuario.js",
+"js/mural/render/Mural_render.js",
+"js/mural/Mural.js",
+"js/cabecalho/novoCartao.js",
+"img/bin2.svg",
+"img/edit.svg"
+]
+
+self.addEventListener('install', function() {
+    console.log('instalou')
+
+    
+
+})
+
+self.addEventListener('activate', function() {
+    
+})
+
+self.addEventListener('fetch', function(event) {
+    let pedido = event.request;
+    console.log('tentou carregar alguma coisa');
+    let promiseResposta = chaches.match(pedido).then(respostaCache => {
+        let resposta = respostaCache ? respostaCache : fetch(pedido)
+        return resposta
+    });
+    
+    event.respondWith(promiseResposta) 
+
+});
+
+
